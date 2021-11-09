598034e4f0861c589fa005bb9ee0d46b10ef287b
Sven Höper <sven@hoeper.me>
2021-11-09 10:41:03 +0100

    setup: install build-essential

--- setup.sh
+++ setup.sh
@@ -7,12 +7,15 @@
 # First, install some dependencies...
 sudo apt-get -y update
 sudo apt-get -y install git tmux
+sudo apt-get -y install build-essential
 sudo apt-get -y install python-matplotlib
 sudo apt-get -y install python-numpy
 
 # Now download and install Mininet
 git clone git://github.com/mininet/mininet
-cd mininet && git checkout -b 2.2.2rc1 && util/install.sh -a
+cd mininet
+git checkout -b 2.2.2rc1 || true
+util/install.sh -a
 
 cd ~
 git clone https://github.com/shoeper/low-rate-tcp-targeted-dos-attacks.git
